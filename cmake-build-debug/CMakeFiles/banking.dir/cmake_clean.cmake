f33e07729bb118c76e878917d1cc05cd21b27ad9
vasinaa <99878647+vasinaa@users.noreply.github.com>
2022-06-01 23:03:11 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/banking.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/banking.dir/banking/Account.cpp.o"
+  "CMakeFiles/banking.dir/banking/Transaction.cpp.o"
+  "libbanking.a"
+  "libbanking.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/banking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/lab05.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab05.dir/banking/Account.cpp.o"
+  "CMakeFiles/lab05.dir/banking/Transaction.cpp.o"
+  "liblab05.a"
+  "liblab05.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
